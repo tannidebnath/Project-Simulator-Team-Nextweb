53c8adf9ea3179a8e659312d2a49133e335a9a7b
abirhussain-shah <abirshah2000.as@gmail.com>
2026-01-22 02:17:33 +0600

    teacher table sql code

---
+++ model/database/teacher_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE teacher (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    teacher_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+) ENGINE=InnoDB;
